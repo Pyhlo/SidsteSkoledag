467998ec368b77f0423e5fa58f983775f8e0011a
Pyhlo <pyhlomail@gmail.com>
2022-05-10 15:37:01 +0200

    update to better checkpoint system

--- src/main/java/me/pyhlo/Checkpoint.java
+++ src/main/java/me/pyhlo/Checkpoint.java
@@ -1,28 +1,56 @@
 package me.pyhlo;
 
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
 
 public class Checkpoint {
-    private int index;
-    private Path songpath;
-    private double startpoint;
-    private double endpoint; //the double is in millieseconds
+    public int index;
+    private int maxIndexes;
+    public String songpath;
+    public String type;
 
-    public double getStartpoint() { //gets the starting point of the current checkpoint
-        return startpoint;
+    public double startpoint;
+    public double endpoint; //the double is in millieseconds
+
+    public Checkpoint(File f, int starting_index) throws IOException {
+        index = starting_index;
+        JsonObject obj = Utils.getElement(f).getAsJsonObject();
+        maxIndexes = obj.size();
+        JsonObject inObj = obj.get(String.valueOf(index)).getAsJsonObject();
+        type = inObj.get("type").getAsString();
+        if (type.equalsIgnoreCase("stop")) {
+            endpoint = inObj.get("end").getAsDouble();
+            songpath = inObj.get("songpath").getAsString();
+        } else {
+            startpoint = inObj.get("start").getAsDouble();
+            endpoint = inObj.get("end").getAsDouble();
+            songpath = inObj.get("songpath").getAsString();
+        }
     }
 
-    public double getEndpoint() { //gets the ending point of the current checkpoint
-        return endpoint;
-    }
-
-    public Path getSongPath() { //gets the path to the song currently being played (if any)
-        return songpath;
-    }
-
-    public int next() { //returns & switches to the next checkpoint
-        return 0;
+    public int next() throws IOException { //returns & switches to the next checkpoint
+        index++;
+        System.out.println("next " + index);
+        JsonObject obj = Utils.getElement(Main.f).getAsJsonObject();
+        System.out.println("before inObj init");
+        JsonObject inObj = obj.get(String.valueOf(index)).getAsJsonObject();
+        System.out.println("before type");
+        type = inObj.get("type").getAsString();
+        System.out.println("still works");
+        if (type.equalsIgnoreCase("stop")) {
+            System.out.println("choose type 'stop'");
+            endpoint = inObj.get("end").getAsDouble();
+            songpath = inObj.get("songpath").getAsString();
+        } else {
+            startpoint = inObj.get("start").getAsDouble();
+            endpoint = inObj.get("end").getAsDouble();
+            songpath = inObj.get("songpath").getAsString();
+        }
+        return index;
     }
 
     public int previous() { //returns & switches to the previous checkpoint
--- src/main/java/me/pyhlo/Events/KeyEvents.java
+++ src/main/java/me/pyhlo/Events/KeyEvents.java
@@ -7,29 +7,45 @@ import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import me.pyhlo.Main;
 
+import java.io.IOException;
+
 public class KeyEvents {
     public static void attachKeyEvents(Scene scene, final Stage stage, MediaPlayer player) {
         scene.setOnKeyPressed((KeyEvent event) -> {
             // Forward the video towards the next checkpoint
             if (event.getCode() == KeyCode.SPACE || event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.ENTER) {
-                System.out.println("NEXT!");
+                if (Main.waitForCommand) {
+                    Main.waitForCommand = false;
+                    Main.player.play();
+                    try {
+                        Main.checkpoint.next();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    /*Main.Multithreading obj = new Main.Multithreading();
+                    obj.start();*/
+                    System.out.println("NEXT!");
+                }
             }
 
             //Previous checkpoint
             else if (event.getCode() == KeyCode.LEFT && event.isShiftDown()) {
+                //find a way to change were we currently are in the video (to rollback the video)
                 System.out.println("PREVIOUS!");
             }
 
             // Pause
             else if (event.getCode() == KeyCode.ESCAPE && event.isShiftDown()){
-                if (Main.paused) {
-                    player.play();
-                    System.out.println("RESUME!");
-                    Main.paused = false;
-                } else {
-                    player.pause();
-                    System.out.println("PAUSE!");
-                    Main.paused = true;
+                if (!Main.waitForCommand){
+                    if (Main.paused) {
+                        player.play();
+                        System.out.println("RESUME!");
+                        Main.paused = false;
+                    } else {
+                        player.pause();
+                        System.out.println("PAUSE!");
+                        Main.paused = true;
+                    }
                 }
             }
         });
